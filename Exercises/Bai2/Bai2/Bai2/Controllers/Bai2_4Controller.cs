81a139acaec810e267f38f34cf49ab115e438252
to-sang <tosang0197@gmail.com>
2022-10-07 16:08:42 +0700

    Bai2 finished

---
+++ Exercises/Bai2/Bai2/Bai2/Controllers/Bai2_4Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bai2.Controllers
+{
+    public class Bai2_4Controller : Controller
+    {
+        // GET: Bai2_4
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
