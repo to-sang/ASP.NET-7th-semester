7a32262cc13ee610f2d7d061c8354e9f18f5b3b0
to-sang <tosang0197@gmail.com>
2022-10-08 11:55:25 +0700

    Bai4_4 finished

---
+++ Exercises/Bai4/Bai4/Bai4/Controllers/Bai4_4Controller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bai4.Controllers
+{
+    public class Bai4_4Controller : Controller
+    {
+        // GET: Bai4_4
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Result()
+        {
+            ViewBag.manv = Request.Form["manv"];
+            int nc = Convert.ToInt32(Request.Form["nc"]);
+            ViewBag.nc = nc;
+            string cv = Request.Form["cv"];
+            double tien = Convert.ToDouble(Request.Form["bl"]) * 650000 *
+                ((nc < 25) ? nc : 2 * nc - 25) + 
+                ((cv == "tp") ? 500000 : (cv == "pp") ? 300000 : 0);
+            ViewBag.tien = tien;
+            return View();
+        }
+    }
+}
