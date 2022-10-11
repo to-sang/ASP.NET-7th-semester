b538d77f46bd54fdb5766d0162ba48d45c880d17
to-sang <tosang0197@gmail.com>
2022-10-11 10:39:34 +0700

    Bai5_2 finished - Bai5 finished

---
+++ Exercises/Bai5/Bai5/Bai5/Controllers/Bai5_2Controller.cs
@@ -0,0 +1,45 @@
+﻿using Bai5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bai5.Controllers
+{
+    public class Bai5_2Controller : Controller
+    {
+        // GET: Bai5_2
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Send(SendMailInfo info)
+        {
+            MailMessage message = new MailMessage(from: info.From, to: info.To);
+            message.SubjectEncoding = System.Text.Encoding.Unicode;
+            message.Subject = info.Subject;
+            message.BodyEncoding = System.Text.Encoding.Unicode;
+            info.Body = info.Body.Replace("\r\n", "<br />");
+            message.Body = info.Body;
+            message.IsBodyHtml = true;
+            SmtpClient client = new SmtpClient(host: "smtp.gmail.com", port: 587)
+            {
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new System.Net.NetworkCredential(info.From, info.Password),
+                EnableSsl = true
+            };
+            try
+            {
+                client.Send(message);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return View("Index");
+        }
+    }
+}
