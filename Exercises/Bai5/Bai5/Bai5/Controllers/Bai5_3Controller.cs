546ca4737033bc714e5d4b5eebcb772d379a9ba4
to-sang <tosang0197@gmail.com>
2022-10-12 00:35:59 +0700

    Update Bai5 - Add Bai5_3

---
+++ Exercises/Bai5/Bai5/Bai5/Controllers/Bai5_3Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bai5.Controllers
+{
+    public class Bai5_3Controller : Controller
+    {
+        // GET: Bai5_3
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
