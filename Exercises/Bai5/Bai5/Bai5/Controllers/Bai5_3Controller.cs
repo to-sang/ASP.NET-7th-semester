9c324be6ec8db75b9dd1d4589bde225d9901074d
to-sang <tosang0197@gmail.com>
2022-10-12 10:21:30 +0700

    Final update Bai5

--- Exercises/Bai5/Bai5/Bai5/Controllers/Bai5_3Controller.cs
+++ Exercises/Bai5/Bai5/Bai5/Controllers/Bai5_3Controller.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Bai5.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,6 +15,45 @@ namespace Bai5.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Send(SendMailInfo info, IEnumerable<HttpPostedFileBase> attachment)
+        {
+            MailMessage message = new MailMessage(from: info.From, to: info.To);
+            message.SubjectEncoding = System.Text.Encoding.Unicode;
+            message.Subject = info.Subject;
+            message.BodyEncoding = System.Text.Encoding.Unicode;
+            info.Body = info.Body.Replace("\r\n", "<br />");
+            message.Body = info.Body;
+            message.IsBodyHtml = true;
+
+            foreach (var file in attachment)
+            {
+                try
+                {
+                    string fileName = Path.GetFileName(file.FileName);
+                    message.Attachments.Add(new Attachment(file.InputStream, fileName));
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+            SmtpClient client = new SmtpClient(host: "smtp.gmail.com", port: 587)
+            {
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new System.Net.NetworkCredential(info.From, info.Password),
+                EnableSsl = true
+            };
+            try
+            {
+                client.Send(message);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return View("Index");
         }
     }
 }
