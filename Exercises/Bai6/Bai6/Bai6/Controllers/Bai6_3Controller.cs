0e6656c60b7d5416f5089e603c9cc75418b534b6
to-sang <tosang0197@gmail.com>
2022-10-13 20:36:32 +0700

    Bai6_3 finished - Bai6 finished

---
+++ Exercises/Bai6/Bai6/Bai6/Controllers/Bai6_3Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bai6.Controllers
+{
+    public class Bai6_3Controller : Controller
+    {
+        // GET: Bai6_3
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
