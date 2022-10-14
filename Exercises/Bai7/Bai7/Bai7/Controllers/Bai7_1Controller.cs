145904bdb55ef0d486d782f54822307e25ef0095
to-sang <tosang0197@gmail.com>
2022-10-14 18:11:39 +0700

    Bai7_1 finished

---
+++ Exercises/Bai7/Bai7/Bai7/Controllers/Bai7_1Controller.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bai7.Controllers
+{
+    public class Bai7_1Controller : Controller
+    {
+        // GET: Bai7_1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string so1, string so2, string cal)
+        {
+            double sot1 = Convert.ToDouble(so1);
+            double sot2 = Convert.ToDouble(so2);
+            switch (cal)
+            {
+                case "Cộng":
+                    ViewBag.kq = sot1 + sot2;
+                    break;
+                case "Trừ":
+                    ViewBag.kq = sot1 - sot2;
+                    break;
+                case "Nhân":
+                    ViewBag.kq = sot1 * sot2;
+                    break;
+                case "Chia":
+                    ViewBag.kq = sot1 / sot2;
+                    break;
+            }
+            ViewBag.kq = ViewBag.kq.ToString();
+            return View();
+        }
+    }
+}
