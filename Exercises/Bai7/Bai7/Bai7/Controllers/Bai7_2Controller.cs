3eb757139dc288c0a5c9fc1939fda67ac4132f9d
to-sang <tosang0197@gmail.com>
2022-10-14 18:48:40 +0700

    Bai7_2 finished - Bai7 finished

---
+++ Exercises/Bai7/Bai7/Bai7/Controllers/Bai7_2Controller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bai7.Controllers
+{
+    public class Bai7_2Controller : Controller
+    {
+        // GET: Bai7_2
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string save)
+        {
+            ViewBag.content = "Full name: " + Request.Form["lname"] + " " + Request.Form["fname"] + "<br />";
+            ViewBag.content += "Email: " + Request.Form["email"] + "<br />";
+            ViewBag.content += "Password: " + Request.Form["pwd"] + "<br />";
+            ViewBag.content += "City: " + Request.Form["city"] + "<br />";
+            ViewBag.content += "Gender: " + Request.Form["gender"];
+            return View();
+        }
+    }
+}
